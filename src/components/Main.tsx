7f21b87bfdb7737bf6d89a9c745a0a780b422e84
PanicP <filmous2136@gmail.com>
2023-04-04 00:54:52 +0200

    add temp navbar

---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from './Navbar'
+
+export const Main: React.FC = () => {
+    return (
+        <div>
+            <Navbar />
+            <div>Main</div>
+        </div>
+    )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+export const Navbar: React.FC = () => {
+    return (
+        <div>
+            <div>PanicP</div>
+            <div>
+                <div>Introduction</div>
+                <div>Work Experiences</div>
+                <div>Education</div>
+                <div>Hobbies</div>
+                <div>Contact</div>
+            </div>
+            <div>
+                <div>Linkedin</div>
+                <div>Email</div>
+            </div>
+        </div>
+    )
+}
